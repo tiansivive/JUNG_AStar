dcec5fdc43fc1f8ca42b892401018c8d117eab8e
Tiago Vila Verde <tiagoandrevilaverde@gmail.com>
2013-03-05 13:31:01 +0000

    Criação do projecto

---
+++ Projecto Projectado sobre os Projectos À sombra da Bananeira/src/Projecto Projectado sobre os Projectos À sombra da Bananeira.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Projecto.cpp
+// Author      : Tiago & Tiago
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
