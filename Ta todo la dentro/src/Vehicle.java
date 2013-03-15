42d4969f621d5b712b221cd468ed9c170b6f95d5
Tiago Pinto <Tiago Pinto@192.168.1.102>
2013-03-15 20:31:28 +0000

    starting vehicle class

---
+++ Ta todo la dentro/src/Vehicle.java
@@ -0,0 +1,23 @@
+
+public class Vehicle {
+	
+	int capacity;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	Vehicle(int cap){
+		capacity = cap;
+	}
+
+	int getCapacity(){
+		return capacity;
+	}
+	
+	void setCapacity(int cap){
+		capacity = cap;
+	}
+	
+}
