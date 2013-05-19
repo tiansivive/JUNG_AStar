d3ea20b7da46eda6269b33a0260f3774139cec22
Tiago Pinto <Tiago Pinto@192.168.1.102>
2013-05-19 23:37:46 +0100

    A-star algorithm and respective states

---
+++ Ta todo la dentro/src/logic/AStar.java
@@ -0,0 +1,54 @@
+package logic;
+
+import java.util.Vector;
+import dataStructure.graph.Vertex;
+
+public class AStar {
+
+	Vector<State> openlist; //to change vector to a priority queue
+	Vector<State> closedlist; //to change vector to a priority queue
+	
+	public AStar() {
+		openlist = new Vector<State>();
+		closedlist = new Vector<State>();
+	}
+	
+	public boolean getPath(Vertex start, Vertex end, Vector<Vertex> toVisit, Vehicle vehicle) {
+		openlist = new Vector<State>();
+		closedlist = new Vector<State>();
+		
+		openlist.add(new State(start, toVisit, vehicle, end));
+		State current;
+		
+		while(!openlist.isEmpty()) {
+			current = openlist.elementAt(0);
+			openlist.remove(0);
+			closedlist.add(current);
+			
+			if(current.isActive()) {
+				if(current.isVertex(end)) {
+					return true; //TODO: change this, return path or current;
+				}
+				
+				Vector<State> neighbor = current.getNeighbor();
+				for (State state_neigh : neighbor) {
+					/* THERE IS NO EQUAL STATES, 'cause fuel
+					// apply score to state_neigh if not applied at getNeightbor
+					boolean found = false;
+					for(State state_close : closedlist) {
+						if(state_neigh == state_close) {
+							found = true;
+							if(state_neigh.score < state_close.score) {
+								change state_close
+							}
+						}
+					}*/
+					openlist.add(state_neigh);
+				}
+			}
+		}
+		return false;
+		
+	}
+
+}
---
+++ Ta todo la dentro/src/logic/State.java
@@ -0,0 +1,184 @@
+package logic;
+
+import java.awt.Point;
+import java.util.Vector;
+
+import dataStructure.graph.Edge;
+import dataStructure.graph.Vertex;
+
+public class State {
+
+	State from;
+	Vertex position;
+	
+	double cost;
+	double heuristic;
+	double total;
+	
+	Vector<Vertex> toVisit;
+	Vector<Integer> order;
+	Vehicle car;
+	
+	//Constructors
+	public State() {
+		from = null;
+		position = null;
+		cost = 0;
+		heuristic = 0;
+		total = 0;
+		toVisit = new Vector<>();
+		car = null;
+	}
+	
+	public State(Vertex pos, Vector<Vertex> toVis, Vehicle veh, Vertex end) {
+		from = null;
+		position = pos;
+		
+		cost = 0;	
+		car = veh;
+		if(isActive() /*&& pos.isFuelStation()*/) { //TODO: change this
+			car.refill();
+		}
+		
+		toVisit = toVis;
+		reCalcToVisit();
+		order = new Vector<Integer>();
+			
+		calcHeurWithoutEnd();
+		heuristic += toVis.elementAt(order.lastElement()).distance(end);
+		calcTotal();
+}
+	
+	public State(State fr, Vertex pos, double cos, Vector<Vertex> toVis, Vehicle veh, Vertex end) {
+			from = fr;
+			position = pos;
+			
+			cost = cos;
+			car = veh;
+			if(isActive() /*&& pos.isFuelStation()*/) { //TODO: change this
+				car.refill();
+			}
+			
+			toVisit = toVis;
+			reCalcToVisit();
+			order = new Vector<Integer>();
+
+			calcHeurWithoutEnd();
+			heuristic += toVis.elementAt(order.lastElement()).distance(end);
+			calcTotal();
+	}
+
+	//Getters
+	public State getFrom() {
+		return from;
+	}
+
+	public Vertex getPosition() {
+		return position;
+	}
+
+	public double getCost() {
+		return cost;
+	}
+
+	public double getHeuristic() {
+		return heuristic;
+	}
+	
+	public double getTotal() {
+		return total;
+	}
+
+	public Vector<Vertex> getToVisit() {
+		return toVisit;
+	}
+
+	public Vehicle getCar() {
+		return car;
+	}
+	
+	
+	
+	private void calcHeurWithoutEnd() {
+		// tested
+		Vertex current = this.position;
+		heuristic = 0;
+		while(order.size() < toVisit.size()) {
+			double minDistance = Double.POSITIVE_INFINITY;
+			int index = 0;
+			
+			for(int i=0; i<toVisit.size(); ++i) {
+				if(!order.contains((Integer)i)){
+					double testDistance = current.distance(toVisit.elementAt(i));
+					if(testDistance < minDistance) {
+						minDistance = testDistance;
+						index = i;
+					}
+				}
+			}
+			
+			order.add(index);
+			heuristic += minDistance;
+			current = toVisit.elementAt(index);
+		}
+		
+	}
+	
+	private void calcTotal() {
+		total = cost + heuristic;
+	}
+	
+	private void reCalcToVisit() {
+		for(int i = 0; i<toVisit.size(); ++i) {
+			if (position.equals(toVisit.elementAt(i))) {
+				toVisit.remove(i);
+				--i;
+			}
+		}
+		
+	}	
+	
+
+	public Vector<State> getNeighbor() {
+		Vector<State> neighbor = new Vector<State>();
+		//TODO: correct this
+		/*
+		Vector<Edge> paths = position.getEdges();
+		
+		for(Edge edge : paths) {
+			Vertex newVertex = new Vertex();
+			
+			if(edge.from.equals.position) {
+				newVertex = edge.to;
+			} else {
+				newVertex = edge.from;
+			}
+			
+			double distance = edge.getDistance();
+			double newCost = this.cost + distance;
+			Vehicle newCar = car.clone();
+			newCar.travel(distance);
+			
+			neighbor.add(new State(this,newVertex,newCost,toVisit,newCar));
+		}
+		*/
+		return neighbor;
+	}
+
+	public boolean isVertex(Vertex vertex) {
+		return position.equals(vertex);
+	}
+
+	public boolean isActive() {
+	
+		if(position == null) {
+			return false;
+		}
+		if(car.getCurrentFuel() < 0) {
+			return false;
+		}
+		
+		return true;
+	}
+
+}
