d7adf3a34ee3224d5749ace639abe5dd78f9623d
Tiago Pinto <Tiago Pinto@192.168.1.102>
2013-05-24 23:42:54 +0100

    added time and distance

--- Ta todo la dentro/src/dataStructure/city/infraStructure/Vehicle.java
+++ Ta todo la dentro/src/dataStructure/city/infraStructure/Vehicle.java
@@ -1,72 +1,84 @@
 package dataStructure.city.infraStructure;
 
 public class Vehicle {
-    
-    
-    private double currentFuel;
-    private double maxCapacity;
-    private double consumption;
 
-    public Vehicle(Vehicle car) {
-            currentFuel = car.getCurrentFuel();
-            maxCapacity = car.getMaxCapacity();
-            consumption = car.getConsumption();
-    }
-    
-    public Vehicle(double start, double max){
-            if(start > max){
-                    currentFuel = max;
-            } else {
-                    currentFuel = start;
-            }
-            maxCapacity = max;
-            consumption = 1;
-    }
-    
-    public Vehicle(double start, double max, double cons){
-            if(start > max){
-                    currentFuel = max;
-            } else {
-                    currentFuel = start;
-            }
-            maxCapacity = max;
-            consumption = cons;
-    }
 
-    public double getCurrentFuel(){
-            return currentFuel;
-    }
-    
-    public double getMaxCapacity(){
-            return maxCapacity;
-    }
-    
-    public double getConsumption() {
-            return consumption;
-    }
-    
-    public void setCurrentFuel(int fuel) {
-            if(fuel > maxCapacity){
-                    currentFuel = maxCapacity;
-            } else {
-                    currentFuel = fuel;
-            }
-    }
-    
-    public void travel(double distance) {
-            currentFuel -= distance*consumption;
-    }
-    
-    public void refill(){
-            currentFuel = maxCapacity;
-    }
-    
-    public void setMaxCapacity(double cap){
-            maxCapacity = cap;
-    }
+	private double currentFuel;
+	private double maxCapacity;
+	private double consumption;
+	private int maxSpeed;
 
-    public void setConsumption(double cons) {
-            consumption = cons;
-    }
-    
+	public Vehicle(Vehicle car) {
+		currentFuel = car.getCurrentFuel();
+		maxCapacity = car.getMaxCapacity();
+		consumption = car.getConsumption();
+		maxSpeed = car.getMaxSpeed();
+	}
+
+	public Vehicle(double start, double max){
+		if(start > max){
+			currentFuel = max;
+		} else {
+			currentFuel = start;
+		}
+		maxCapacity = max;
+		consumption = 1;
+		maxSpeed = 100;
+	}
+
+	public Vehicle(double start, double max, double cons, int speed){
+		if(start > max){
+			currentFuel = max;
+		} else {
+			currentFuel = start;
+		}
+		maxCapacity = max;
+		consumption = cons;
+		maxSpeed = speed;
+	}
+
+	public double getCurrentFuel(){
+		return currentFuel;
+	}
+
+	public double getMaxCapacity(){
+		return maxCapacity;
+	}
+
+	public double getConsumption() {
+		return consumption;
+	}
+
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setCurrentFuel(int fuel) {
+		if(fuel > maxCapacity){
+			currentFuel = maxCapacity;
+		} else {
+			currentFuel = fuel;
+		}
+	}
+
+	public void travel(double distance) {
+		currentFuel -= distance*consumption;
+	}
+
+	public void refill(){
+		currentFuel = maxCapacity;
+	}
+
+	public void setMaxCapacity(double cap){
+		maxCapacity = cap;
+	}
+
+	public void setConsumption(double cons) {
+		consumption = cons;
+	}
+	
+	public void setMaxSpeed(int speed) {
+		maxSpeed = speed;
+	}
+
 }
